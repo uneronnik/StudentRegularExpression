acfe475e7e5569ce93ea8f9243519b73dd3fb70c
uneronnik <NIKI.FOMENKO.7@GMAIL.COM>
2021-10-08 00:04:46 +0300

    Initial commit

---
+++ StudentRegularExpression/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegularExpression
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            try
+            {
+                student.FIO = "Фоменко никита";
+            }catch(Exception ex) { Console.WriteLine(ex.Message + "\n"); }
+            student.FIO = "Фоменко Никита Александрович";
+            Console.WriteLine(student + "\n");
+
+            try
+            {
+                student.PhoneNumber = "+7 56 4";
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message + "\n"); }
+            student.PhoneNumber = "+75437548995";
+            Console.WriteLine(student + "\n");
+            student.PhoneNumber = "+725437548995";
+            Console.WriteLine(student + "\n");
+
+            try
+            {
+                student.AvgMark = "13";
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message + "\n"); }
+            student.AvgMark = "9";
+            Console.WriteLine(student + "\n");
+            student.AvgMark = "5";
+            Console.WriteLine(student + "\n");
+            student.AvgMark = "2";
+            Console.WriteLine(student + "\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StudentRegularExpression/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StudentRegularExpression
+{
+    class Student
+    {
+        string _FIO;
+        string _phoneNumber;
+        string _avgMark;
+        public string FIO
+        {
+            get => _FIO;
+            set
+            {
+                string FIOPattern = @"^([А-Я][а-я]+\s){2}([А-Я][а-я]+)$";
+                if (!Regex.IsMatch(value, FIOPattern))
+                    throw new Exception("Incorrect FIO");
+                _FIO = value;
+            }
+        }
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                string PhonePattern = @"^\+\d{1,2}\d{10}$";
+                if (!Regex.IsMatch(value, PhonePattern))
+                    throw new Exception("Incorrect Number");
+                _phoneNumber = value;
+            }
+        }
+        public string AvgMark
+        {
+            get => _avgMark;
+            set
+            {
+                string MarkPattern = @"^([1-9]|1[0-2])$";
+                if (!Regex.IsMatch(value, MarkPattern))
+                    throw new Exception("Incorrect Mark");
+                _avgMark = value;
+            }
+        }
+        public string Scholarship
+        {
+            get 
+            {
+                int avgMark = Convert.ToInt32(AvgMark);
+                if (avgMark <= 4)
+                    return 0.ToString();
+                else if (avgMark <= 8)
+                    return 5000.ToString();
+                else
+                    return 10000.ToString();
+            }
+        }
+        public override string ToString()
+        {
+
+            return $"FIO: {FIO}\n" +
+                   $"Phone number: {PhoneNumber}\n" +
+                   $"Average mark: {AvgMark}\n" +
+                   $"Scholarship: {Scholarship}";
+        }
+
+    }
+}
